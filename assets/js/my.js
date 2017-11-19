cec8ef2dc24cae13cb490b0c629ea44039f94590
degtra <e.p.deltian@gmail.com>
2017-11-19 11:22:36 +0700

    miniprojek

--- assets/js/my.js
+++ assets/js/my.js
@@ -13,17 +13,28 @@ $(window).scroll(function () {
     $('.jumbotron p').css({
         'transform' : 'translate(0px, '+wscroll/4+'%)'
     });
-    
-    // konten
 });
 
-// paralax about
+// paralax about,poto
 
 $(window).on('load', function () {
     $('.pkiri').addClass('pnimbul');
     $('.pkanan').addClass('pnimbul');  
     $('.gambar').addClass('muncul');  
 });
+
+// map
+function initMap() {
+    var location = {lat: -6.898032, lng: 107.654965};
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 16,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+};
 
 
 // stop video
